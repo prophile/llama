5e05e409100b119e5f3f687b6679eb5bf7585d74
Alastair Lynn <arplynn@gmail.com>
2010-10-17 15:22:05 +0100

    Made page size fetching non-lazy.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- llama-mmap.c
+++ llama-mmap.c
@@ -16,8 +16,11 @@ static int permissions_to_prot(llama_permissions_t permissions)
 	return result;
 }
 
+static size_t pageSize = 0;
+
 void llama_init(void)
 {
+	pageSize = getpagesize();
 }
 
 void llama_quit(void)
@@ -31,33 +34,33 @@ llama_options_t llama_supported_options(void)
 
 size_t llama_page_size(void)
 {
-	MEMOISE(size_t, getpagesize());
+	return pageSize;
 }
 
 void* llama_allocate(unsigned pageCount, llama_permissions_t permissions,
                      llama_options_t options, void* fixedAddress)
 {
-	return mmap(fixedAddress, pageCount * llama_page_size(),
+	return mmap(fixedAddress, pageCount * pageSize,
 	            permissions_to_prot(permissions), MAP_ANONYMOUS | MAP_PRIVATE,
 	            0, 0);
 }
 
 void llama_deallocate(void* base, unsigned pageCount)
 {
-	munmap(base, pageCount * llama_page_size());
+	munmap(base, pageCount * pageSize);
 }
 
 bool llama_lock(void* base, unsigned pageCount)
 {
-	return mlock(base, pageCount * llama_page_size()) == 0;
+	return mlock(base, pageCount * pageSize) == 0;
 }
 
 void llama_unlock(void* base, unsigned pageCount)
 {
-	munlock(base, pageCount * llama_page_size());
+	munlock(base, pageCount * pageSize);
 }
 
 void llama_set_permissions(void* base, unsigned pageCount, llama_permissions_t newPermissions)
 {
-	mprotect(base, pageCount * llama_page_size(), permissions_to_prot(newPermissions));
+	mprotect(base, pageCount * pageSize, permissions_to_prot(newPermissions));
 }
