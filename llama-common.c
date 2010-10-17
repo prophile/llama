2a04b1f002951f8463b3178368d31076bed90b36
Alastair Lynn <arplynn@gmail.com>
2010-10-17 15:04:35 +0100

    Added llama-common, implementing common functionality.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ llama-common.c
@@ -0,0 +1,16 @@
+#include "llama.h"
+
+int llama_version_major(void)
+{
+	return LLAMA_VERSION_MAJOR;
+}
+
+int llama_version_minor(void)
+{
+	return LLAMA_VERSION_MINOR;
+}
+
+int llama_version_patch(void)
+{
+	return LLAMA_VERSION_PATCH;
+}
